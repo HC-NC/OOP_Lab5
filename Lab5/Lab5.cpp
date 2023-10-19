a992d0a41a29882e28f025710fe8f69cc6b27c71
Alexander Dubrovsky <sanya.dubrovskiy@inbox.ru>
2023-10-19 21:58:28 +0700

    init

---
+++ Lab5/Lab5.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    
+    const int N = 10;
+
+    float a[N];
+
+    cout << "Введите " << N << " чисел:" << endl;
+
+    for (int i = 0; i < N; i++)
+        cin >> a[i];
+
+    int k;
+
+    cout << "Введите k (>=2): ";
+
+    cin >> k;
+
+    int count = 0;
+
+    for (int i = 0; i < N; i++)
+    {
+        if (i % k == 0)
+            a[i] = 0;
+        else
+            count++;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        if (a[i] != 0)
+            cout << a[i] << " ";
+    }
+
+    cout << "\nКоличество оставшихся: " << count << endl;
+
+    return 0;
+}
